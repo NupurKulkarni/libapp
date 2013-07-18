c99db12e20fefd3c9183b1bc064cb95a344d8687
Nupur <nihalkulkarni@ymail.com>
2013-07-18 17:26:18 +0530

    New reader view spec

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Libapp::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
   get "/register", to: "readers#new", as: "register"
+  
+  resources :readers
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
